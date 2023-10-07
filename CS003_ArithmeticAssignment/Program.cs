c90bed4b91078f69f0e2224fb1f9a8451111fe91
Hai Pham <ngochai285nd@gmail.com>
2023-10-07 16:01:55 +0700

    update CS003

---
+++ CS003_ArithmeticAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CS003_ArithmeticAssignment;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        // Declare variables
+        decimal a, b;
+
+        // Get input from user
+        Console.Write("Enter a: ");
+        a = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Enter b: ");
+        b = Convert.ToDecimal(Console.ReadLine());
+
+        // Display result
+        Console.WriteLine("a + b = " + (a + b));
+        Console.WriteLine("a - b = " + (a - b));
+        Console.WriteLine("a * b = " + (a * b));
+        Console.WriteLine("a / b = " + (a / b));
+        Console.WriteLine("a % b = " + (a % b));
+        Console.WriteLine("a++ = " + (a++));
+        Console.WriteLine("a-- = " + (a--));
+        Console.WriteLine("++a = " + (++a));
+        Console.WriteLine("--a = " + (--a));
+    }
+}
